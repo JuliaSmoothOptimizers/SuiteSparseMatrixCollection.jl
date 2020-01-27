538e68edee72169c149b2202aac05ddfdb233485
Dominique Orban <dominique.orban@gmail.com>
2020-01-27 18:07:44 -0500

    add convenience functions, improve docs

--- src/SuiteSparseMatrixCollection.jl
+++ src/SuiteSparseMatrixCollection.jl
@@ -4,7 +4,7 @@ __precompile__(false)
 using JuliaDB
 using Dates
 
-export fetch_ssmc, group_path, matrix_path, ssmc, ssmc_dir, ssmc_formats
+export fetch_ssmc, ssmc_matrices, group_path, matrix_path, ssmc, ssmc_dir, ssmc_formats
 
 const colnames = [
   "id",
@@ -88,8 +88,18 @@ matrix_path(matrix; format="MM") = joinpath(group_path(matrix; format=format), m
 """
     fetch_ssmc(matrices; format="MM")
 
-Download matrices from the SuiteSparseMatrixCollection.
+If `matrices` is iterable, download matrices from the SuiteSparseMatrixCollection.
 Each matrix will be stored in `matrix_path(matrix; format=format)`.
+
+    fetch_ssmc(name; format="MM")
+
+If `name` is a string, select matrices whose name contain the string
+`name` in the collection, and download them.
+
+    fetch_ssmc(group, name; format="MM")
+
+If `group` and `name` are strings, select matrices whose group and name contain the strings
+`group` and `name`, respectively, in the collection, and download them.
 """
 function fetch_ssmc(matrices; format="MM")
   format ∈ ssmc_formats || error("unknown format $format")
@@ -99,13 +109,42 @@ function fetch_ssmc(matrices; format="MM")
     g_path = group_path(matrix, format=format)
     mkpath(g_path)
     fname = joinpath(g_path, "$(matrix.name).$(ext)")
-    isfile(fname) || download(url, fname)
+    if !isfile(fname)
+      @info "downloading $fname"
+      download(url, fname)
+    end
     if ext == "tar.gz"
       tar_options = Sys.iswindows() ? "--force-local" : ""
       run(`tar $tar_options -zxf $fname -C $g_path`)
       rm(fname)
     end
   end
+end
+
+function fetch_ssmc(group::AbstractString, name::AbstractString; kwargs...)
+  matrices = ssmc_matrices(group, name)
+  fetch_ssmc(matrices; kwargs...)
+  return matrices
+end
+
+"""
+    ssmc_matrices(group, name)
+
+Return an iterable of matrices whose group contains the string `group` and whose
+name contains the string `name`.
+
+    ssmc_matrices("", name)
+
+Return an iterable of matrices whose name contains the string `name`.
+
+    ssmc_matrices(group, "")
+
+Return an iterable of matrices whose group contains the string `group`.
+
+Example: `ssmc_matrices("HB", "bcsstk")`.
+"""
+function ssmc_matrices(group::AbstractString, name::AbstractString)
+  filter(p -> occursin(group, p.group) && occursin(name, p.name), ssmc)
 end
 
 end
