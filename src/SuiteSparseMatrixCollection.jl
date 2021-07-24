777b081c852570b5bf9e8fab4a2fc2a7b8c0f9f6
Dominique <dominique.orban@gmail.com>
2021-07-23 21:02:56 -0400

    Update src/SuiteSparseMatrixCollection.jl
    Co-authored-by: Alexis <35051714+amontoison@users.noreply.github.com>

--- src/SuiteSparseMatrixCollection.jl
+++ src/SuiteSparseMatrixCollection.jl
@@ -56,7 +56,7 @@ end
 Return a `DataFrame` of matrices whose group contains the string `group` and whose
 name contains the string `name`.
 
-    ssmc_matrices(ssmc name)
+    ssmc_matrices(ssmc, name)
     ssmc_matrices(ssmc, "", name)
 
 Return a `DataFrame` of matrices whose name contains the string `name`.
