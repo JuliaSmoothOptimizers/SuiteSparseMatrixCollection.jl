35323a189a71df70ec31f4d69d3a3e15be63417c
amontoison <amontoison@users.noreply.github.com>
2021-08-24 20:17:35 +0000

    :robot: Format .jl files

--- src/SuiteSparseMatrixCollection.jl
+++ src/SuiteSparseMatrixCollection.jl
@@ -5,7 +5,8 @@ using Pkg.Artifacts
 using DataFrames
 using JLD2
 
-export ssmc_db, fetch_ssmc, ssmc_matrices, ssmc, ssmc_formats, installed_ssmc, delete_ssmc, delete_all_ssmc
+export ssmc_db,
+  fetch_ssmc, ssmc_matrices, ssmc, ssmc_formats, installed_ssmc, delete_ssmc, delete_all_ssmc
 
 const ssmc_jld2 = joinpath(@__DIR__, "..", "src", "ssmc.jld2")
 const ssmc_artifacts = joinpath(@__DIR__, "..", "Artifacts.toml")
