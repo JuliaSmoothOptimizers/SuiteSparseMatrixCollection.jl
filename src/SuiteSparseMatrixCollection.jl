8e641ae6258f834d9b01a2a2db214b7cc0381f0a
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-01-16 14:23:21 +0100

    Fix Aqua workflow

--- src/SuiteSparseMatrixCollection.jl
+++ src/SuiteSparseMatrixCollection.jl
@@ -6,7 +6,7 @@ using DataFrames
 using JLD2
 
 export ssmc_db,
-  fetch_ssmc, ssmc_matrices, ssmc, ssmc_formats, installed_ssmc, delete_ssmc, delete_all_ssmc
+  fetch_ssmc, ssmc_matrices, ssmc_formats, installed_ssmc, delete_ssmc, delete_all_ssmc
 
 const ssmc_jld2 = joinpath(@__DIR__, "..", "src", "ssmc.jld2")
 const ssmc_artifacts = joinpath(@__DIR__, "..", "Artifacts.toml")
