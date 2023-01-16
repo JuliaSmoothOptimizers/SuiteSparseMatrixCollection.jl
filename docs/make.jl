32eb411f839c492f7dacf54bbc79dadc77110148
amontoison <amontoison@users.noreply.github.com>
2023-01-16 10:32:50 +0000

    :robot: Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -5,9 +5,11 @@ makedocs(
   doctest = true,
   linkcheck = true,
   strict = true,
-  format = Documenter.HTML(assets = ["assets/style.css"],
-                           prettyurls = get(ENV, "CI", nothing) == "true",
-                           ansicolor = true),
+  format = Documenter.HTML(
+    assets = ["assets/style.css"],
+    prettyurls = get(ENV, "CI", nothing) == "true",
+    ansicolor = true,
+  ),
   sitename = "SuiteSparseMatrixCollection.jl",
   pages = ["Home" => "index.md", "Reference" => "reference.md"],
 )
